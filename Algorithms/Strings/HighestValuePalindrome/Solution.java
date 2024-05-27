7cf73ae748ee3323843ef75bdedd20bde9585608
Tyler C de Laguna <71159192+profinite@users.noreply.github.com>
2024-05-26 23:07:52 -0700

    Update Solution.java

--- Algorithms/Strings/HighestValuePalindrome/Solution.java
+++ Algorithms/Strings/HighestValuePalindrome/Solution.java
@@ -5,40 +5,41 @@
  * Fairly straight-forward
  */
 class Result {
-    public static String highestValuePalindrome(String s, int n, int k) {
-        final String FAIL = "-1";
-        StringBuilder result = new StringBuilder(s);
-        int oddCount = palindromeCount(s);
-        if(oddCount > k) return FAIL;
-        int surplus = k - oddCount;
+   public static String highestValuePalindrome(String s, int n, int k) {
+        int surplus = budgetOf(s, k);
+        if(surplus < 0)
+            return "-1";
+        StringBuilder largest = new StringBuilder(s);
         for(int i = 0, j = n - 1; i < n / 2; i++, j--) {
-            char start = s.charAt(i);
-            char end = s.charAt(j);
-            char larger = (char) Math.max((int)start, (int)end);
-            if (start == end)
-                if(start != '9' && surplus >= 2) {
-                    larger = '9';
+            int first = Character.getNumericValue(s.charAt(i));
+            int last = Character.getNumericValue(s.charAt(j))
+            int larger = Math.max(first, last);
+            if(first == last) {
+                if(first != 9 && surplus >= 2) {
+                    larger = 9;
                     surplus -= 2;
                 }
-            else if(surplus > 0 && larger != '9') {
-                larger = '9';
+            } else if (surplus > 0 && larger != 9) {
+                larger = 9;
                 surplus--;
-                oddCount--;
             }
-            else {
-                oddCount--;
-            }
-            result.setCharAt(i, larger);
-            result.setCharAt(j, larger);
+            largest.setCharAt(i, Character.forDigit(larger, 10));
+            largest.setCharAt(j, Character.forDigit(larger, 10));
         }
-        return result.toString();
+        if(n % 2 == 1 && surplus > 0) {
+            int middle = n / 2;
+            largest.setCharAt(middle, '9');
+        } 
+        return largest.toString();
     }
-    static private int palindromeCount(String s) {
-        int count = 0;
-        for(int i = 0, j = s.length() - 1; i < j / 2; i++, j--) {
-            if(s.charAt(i) != s.charAt(j))
-                count++;
+    static int budgetOf(String s, int k) {
+        int n = s.length();
+        for(int i = 0, j = n - 1; i < n / 2; i++, j--) {
+            int first = Character.getNumericValue(s.charAt(i));
+            int last = Character.getNumericValue(s.charAt(j))
+            if(first != last)
+                k--;
         }
-        return count;
+        return k;
     }
 }
