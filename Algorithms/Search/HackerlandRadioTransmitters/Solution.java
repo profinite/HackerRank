54308d63359ebeed5b78497c75b3459be03edcb6
Tyler C de Laguna <71159192+profinite@users.noreply.github.com>
2024-05-26 21:31:53 -0700

    Create Solution.java

---
+++ Algorithms/Search/HackerlandRadioTransmitters/Solution.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /**
+     * Find the minimum radio antennae to cover the given
+     * houses each at a given distance.
+     * https://www.hackerrank.com/challenges/hackerland-radio-transmitters/
+     *
+     * Applies basic sliding window, with a BST to find
+     * the optimal house within range for installation.
+     *
+     * 0(n log n) runtime complexity due to sorting
+     *
+     * @param x house locations
+     * @return minimum antenna stations
+     */
+    public static int hackerlandRadioTransmitters(List<Integer> x, int k) {
+        TreeMap<Integer, Boolean> atlas = new TreeMap<>();
+        for(Integer index : x)
+            atlas.put(index, false);
+        int count = 0;
+        Collections.sort(x);
+        for(Integer index : x) {
+            if(atlas.get(index))
+                continue;
+            int furthest = atlas.floorKey(index + k);
+            atlas.subMap(index, furthest + k + 1).replaceAll((a, b) -> true);
+            count++;
+        }
+        return count;
+    }
+
+    public static int hackerlandRadioTransmitters2(List<Integer> x, int k) {
+        // standard sliding window I guess...
+        Collections.sort(x);
+        int count = 0;
+        int firstHouse = -1;
+        System.err.println(x);
+        for(Integer index : x) {
+            if(firstHouse == -1 || index > firstHouse + 2 * k) {
+                firstHouse = index;
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> x = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.hackerlandRadioTransmitters(x, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
