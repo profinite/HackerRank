99e462eac73b5f4201513cd4eb32a49960c42f4f
Tyler C de Laguna <71159192+profinite@users.noreply.github.com>
2024-08-16 14:18:02 -0700

    Update Solution.java

--- Algorithms/Constructive/NewYearChaos/Solution.java
+++ Algorithms/Constructive/NewYearChaos/Solution.java
@@ -6,19 +6,20 @@
      * @param q final ordering of the queue.
      *          ----
      *          Steps over queue of people from front to end, counting those whose rank exceeds
-     *          the current person. Reports if jump exceeds a certain threshold.
-     *          <p>
+     *          the current person. Reports if jump exceeds a certain threshold. 1-based index.
+     *          
      *          𝚯(n log n) runtime
      *          𝚯(n) space
      */
     public static void minimumBribes(List<Integer> q) {
-        TreeSet<Integer> sorted = new TreeSet<>();
+        TreeSet<Integer> observed = new TreeSet<>();
         long bribes = 0;
-        int index = 0;
+        int index = 1;
         for (Integer person : q) {
-            bribes += sorted.tailSet(person, false).size();
-            sorted.add(person);
-            final int MAX_JUMP = 3;
+            // Count the queue-jumpers observed ahead of this person.
+            bribes += observed.tailSet(person, false).size();
+            observed.add(person);
+            final int MAX_JUMP = 2;
             if (person - index > MAX_JUMP) {
                 System.out.println("Too chaotic");
                 System.out.flush();
